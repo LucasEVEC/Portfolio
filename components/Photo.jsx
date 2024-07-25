dcecb2de17d4bd24fddafe0c9d214cf690857a76
Lucas EVEC <lucasevecdev@gmail.com>
2024-07-25 21:39:40 +0200

    test02

--- components/Photo.jsx
+++ components/Photo.jsx
@@ -1,59 +1,19 @@
 "use client";
 
-import { motion } from "framer-motion";
-import Image from "next/image";
-
 const Photo = () => {
   return (
     <div className="flex justify-center items-center w-full h-full relative">
-      <motion.div 
-        initial={{opacity:0}}
-        animate={{
-            opacity: 1,
-            transition: { delay: 2, duration: 0.4, ease: "easeIn"},
-            }}
-      >
-        {/* Simplified Image without gradient mask */}
-        <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] rounded-full absolute overflow-hidden">
-          <Image 
-            src="/assets/unnamed.jpg"
-            priority
-            quality={100}
-            layout="fill"
-            alt="Photo de profil"
-            className="object-cover"
-          />
-        </div>
-
-        {/* Circle Animation */}
-        <motion.svg 
-            className="w-[300px] xl:w-[506px] h-[300px] xl:h-[506px]" fill="transparent"
-            viewBox="0 0 506 506"
-            xmlns="http://www.w3.org/2000/svg"
-        >
-            <motion.circle
-                cx="253"
-                cy="253"
-                r="250"
-                stroke="#00ff99"
-                strokeWidth="4"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                initial={{strokeDasharray: "24 10 0 0"}}
-                animate={{
-                    strokeDasharray:["15 120 25 25", "16 25 92 72", "4 250 22 22"],
-                    rotate: [120, 360],
-                }}
-                transition={{
-                    duration:20,
-                    repeat: Infinity,
-                    repeatType: "reverse",
-                }}
-            />
-        </motion.svg>
-      </motion.div>
+      <div className="w-[298px] h-[298px] xl:w-[498px] xl:h-[498px] rounded-full overflow-hidden flex justify-center items-center relative">
+        <img 
+          src="/assets/profile-picture.jpg"
+          alt="Photo de profil"
+          className="object-cover w-full h-full absolute inset-0"
+        />
+      </div>
     </div>
   );
 };
 
 export default Photo;
+
+
